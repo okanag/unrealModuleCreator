3506316920ab8c86419ae038561f6ed6c6d7ceec
Okan Agca <okanagca@gmail.com>
2017-09-10 22:47:12 +0200

    update assembly info for release

--- ue4ModuleCreator/Properties/AssemblyInfo.cs
+++ ue4ModuleCreator/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ue4ModuleCreator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An automated module creation tool for Unreal Engine 4")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ue4ModuleCreator")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
